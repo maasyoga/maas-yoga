dc823667329148411d5e879f6480a9f18cc8cf02
Agus Casari <agustincasari@veoestudio.com>
2023-01-21 19:24:42 -0300

    s

--- frontend/src/pages/colleges.jsx
+++ frontend/src/pages/colleges.jsx
@@ -4,9 +4,11 @@ export default function Colleges(props) {
 
     return(
         <>
-            <div className="bg-red-100 border-l-4 border-red-500 text-red-700 p-4" role="alert">
-                <p className="font-bold">Sedes</p>
-                <p>Sedes</p>
+            <div className="bg-white rounded-3xl p-8 mb-5 mt-6 md:mt-16">
+                <div className="bg-red-100 border-l-4 border-red-500 text-red-700 p-4" role="alert">
+                    <p className="font-bold">Sedes</p>
+                    <p>Sedes</p>
+                </div>
             </div>
         </>
     );
