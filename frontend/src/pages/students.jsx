4ec4fa174e6c3aa01ca312468b15a8869c5d4b9f
Agus Casari <agustincasari@veoestudio.com>
2023-01-29 14:13:26 -0300

    new student modal added

--- frontend/src/pages/students.jsx
+++ frontend/src/pages/students.jsx
@@ -2,11 +2,40 @@ import React, {useState} from "react";
 import AddIcon from '@mui/icons-material/Add';
 import { orange } from '@mui/material/colors';
 import Modal from "../components/modal";
+import SchoolIcon from '@mui/icons-material/School';
+import { useFormik } from 'formik';
+import CommonInput from "../components/commonInput";
 
 export default function Students(props) {
 
     const [displayModal, setDisplayModal] = useState(false);
-    const white = orange[50];
+    const [isLoading, setIsLoading] = useState(false);
+    const setDisplay = (value) => {
+        setDisplayModal(value);
+    }
+
+    const formik = useFormik({
+        initialValues: {
+          email: '',
+          password: '',
+        },
+        onSubmit: async (values) => {
+          const body = {
+            email: values.email,
+            password: values.password,
+          };
+          setIsLoading(true);
+          try {
+           // const response = await authUser.authUser(body);
+           console.log(body)
+            setIsLoading(true);
+          } catch (error) {
+            setIsLoading(false);
+          }
+        },
+      });
+
+    /*const white = orange[50];*/
 
     return(
         <>
@@ -17,11 +46,86 @@ export default function Students(props) {
                     <p>Alumnos</p>
                 </div>
                 <div className="flex justify-end">
-                    <button onclick={() => setDisplayModal(true)}
-                            class="mt-6 bg-yellow-900 w-14 h-14 rounded-full shadow-lg flex justify-center items-center text-white text-4xl transition duration-200 ease-in-out bg-none hover:bg-none transform hover:-translate-y-1 hover:scale-115"><span className="font-bold text-sm text-yellow-900"><AddIcon fontSize="large" sx={{ color: orange[50] }} /></span>
+                    <button onClick={() => setDisplayModal(true)}
+                            className="mt-6 bg-yellow-900 w-14 h-14 rounded-full shadow-lg flex justify-center items-center text-white text-4xl transition duration-200 ease-in-out bg-none hover:bg-none transform hover:-translate-y-1 hover:scale-115"><span className="font-bold text-sm text-yellow-900"><AddIcon fontSize="large" sx={{ color: orange[50] }} /></span>
                     </button>
                 </div>
-                {displayModal && (<><Modal /></>)}
+                <Modal icon={<SchoolIcon />} open={displayModal} setDisplay={setDisplay} title="Agregar alumno" buttonText={isLoading ? (<><i className="fa fa-circle-o-notch fa-spin"></i><span className="ml-2">Agregando...</span></>) : <span>Agregar</span>} children={<>
+                    <form className="pr-8 pt-6 mb-4"    
+                        method="POST"
+                        id="form"
+                        onSubmit={formik.handleSubmit}
+                    >
+                        <div className="grid grid-cols-2 gap-4">
+                            <div className="mb-4">
+                                <CommonInput 
+                                    label="Nombre"    
+                                    onBlur={formik.handleBlur}
+                                    value={formik.values.name}
+                                    name="name"
+                                    htmlFor="name"
+                                    id="name" 
+                                    type="text" 
+                                    placeholder="Nombre" 
+                                    onChange={formik.handleChange}
+                                />
+                            </div>
+                            <div className="mb-4">
+                            <CommonInput 
+                                    label="Apellido"    
+                                    onBlur={formik.handleBlur}
+                                    value={formik.values.surname}
+                                    name="surname"
+                                    htmlFor="surname"
+                                    id="surname" 
+                                    type="text" 
+                                    placeholder="Apellido"
+                                    onChange={formik.handleChange}
+                            />
+                            </div>
+                            <div className="mb-4">
+                            <CommonInput 
+                                    label="Documento"    
+                                    onBlur={formik.handleBlur}
+                                    value={formik.values.document}
+                                    name="document"
+                                    htmlFor="document"
+                                    id="document" 
+                                    type="number" 
+                                    placeholder="Documento"
+                                    onChange={formik.handleChange}
+                            />
+                            </div>
+                            <div className="mb-4">
+                                <CommonInput 
+                                    label="Email"    
+                                    onBlur={formik.handleBlur}
+                                    value={formik.values.email}
+                                    name="email"
+                                    htmlFor="email"
+                                    id="email" 
+                                    type="text" 
+                                    placeholder="Email" 
+                                    onChange={formik.handleChange}
+                                />
+                            </div>
+                            <div className="mb-4">
+                            <CommonInput 
+                                    label="Numero de telefono"    
+                                    onBlur={formik.handleBlur}
+                                    value={formik.values.phoneNumber}
+                                    name="phoneNumber"
+                                    htmlFor="phoneNumber"
+                                    id="phoneNumber" 
+                                    type="number" 
+                                    placeholder="Numero de telefono"
+                                    onChange={formik.handleChange}
+                            />
+                            </div>
+                        </div>
+                    </form>
+                </>
+                } />
             </div>    
         </>
     );
