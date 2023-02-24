e012c36f8f9ec478d615c87802d4555c546152dc
Tomas Arras <tomasarras@gmail.com>
2023-02-24 20:32:42 -0300

    feat(frontend): close icon in modal component

--- frontend/src/components/modal.jsx
+++ frontend/src/components/modal.jsx
@@ -1,4 +1,5 @@
-import React, { useRef } from 'react'
+import React, { useRef } from 'react';
+import CloseIcon from '@mui/icons-material/Close';
 
 export default function Modal(props) {
   
@@ -18,17 +19,22 @@ export default function Modal(props) {
           <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
               <div className="relative transform rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg" style={props.style}>
                 <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-                  <div className="sm:flex sm:items-start">
-                    <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-orange-100 sm:mx-0 sm:h-10 sm:w-10">
-                      {props.icon}
+                  <div className="flex flex-col">
+                    <div className="modal-header w-full flex justify-between">
+                      <div className="flex items-center">
+                        <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-orange-100 sm:mx-0 sm:h-10 sm:w-10">
+                          {props.icon}
+                        </div>
+                        <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                          <div as="h3" className="text-lg font-medium leading-6 text-gray-900">
+                            {props.title}
+                          </div>
+                        </div>
+                      </div>
+                      <CloseIcon className="cursor-pointer" onClick={onClose}/>
                     </div>
-                    <div className="mt-3 text-center w-full sm:mt-0 sm:ml-4 sm:text-left">
-                      <div as="h3" className="text-lg font-medium leading-6 text-gray-900">
-                        {props.title}
-                      </div>
-                      <div className="mt-2">
+                    <div className="mt-2">
                       {props.children}
-                      </div>
                     </div>
                   </div>
                 </div>
